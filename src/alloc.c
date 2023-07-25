3d58f14f3937024a06caed0152bcd7a8493259bc
Fleta Marcia <fletamar@va-h1.kzn.21-school.ru>
2023-07-25 16:42:21 +0300

    src

---
+++ src/alloc.c
@@ -0,0 +1,19 @@
+#include "functions.h"
+
+static char allocbuf[ALLOCSIZE];    /* storage for alloc */
+static char *allocp = allocbuf;     /* next free position */
+
+char *alloc(int n) /* return pointer to n characters */
+{
+    if (allocbuf + ALLOCSIZE - allocp >= n) {   /*it fits */
+        allocp += n;
+        return allocp - n;  /* old p */
+    } else      /* not enough space */
+        return 0;
+}   
+
+void afree(char* p) /* free storage pointed to by p */
+{
+    if (p >= allocbuf && p < allocbuf + ALLOCSIZE)
+        allocp = p;
+}
--- src/functions.h
+++ src/functions.h
@@ -1,6 +1,7 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 
+#define ALLOCSIZE 10000 /* size of available space */
 #define MAXLEN 1000 /* max length of any input line */
 #define MAXLINES 5000 /* max #lines to be sorted */
 
