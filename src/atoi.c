ac67df1a151ae9c4b2ef3dddb66b5ab80ef55267
Fleta Marcia <fletamar@va-h1.kzn.21-school.ru>
2023-08-13 16:46:11 +0300

    lower && atoi functions

--- src/atoi.c
+++ src/atoi.c
@@ -1,7 +1,16 @@
 double atof(char s[]);
 
 /* atoi: convert string s to integer using atof */
-int atoi(char s[])
-{    return (int) atof(s);}
+/* int atoi(char s[])
+/* {    return (int) atof(s);}
 
+/* atoi: convert s to integer */
+int atoi(char *s)
+{
+    int i, n;
 
+    n = 0;
+    for (i = 0; s[i] >= '0' && s[i] <= '9'; ++i)
+        n = 10 * n + (s[i] - '0');
+    return n;
+}
